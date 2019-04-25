daea4cc3dcd2dd6d80dbfeb0bf9cac0d0477a1f5
Yamazaki Satoshi <s2node@gmail.com>
2019-04-25 13:21:30 +0900

    WiFi connect and Ambient upload

--- DHT12.ino
+++ DHT12.ino
@@ -1,9 +1,60 @@
+
+/*
+M5Stack で DHT12を表示しつつ Ambient にデータを送信する
+
+ToDo:
+
+□目盛りの数値描画
+□ボタンで表示モード切替
+□ボタンでメーターの上下とゼロリセット
+□ボタンでスケール変更
+☑温度アナログメータ
+□温度によって色を変える。
+□湿度アナログメータ
+□湿度バーのメータ
+□横軸を時間でグラフ表示を出す
+☑WiFi接続
+☑Ambientデータ送信
+
+*/
+
+// https://github.com/AmbientDataInc/Ambient_ESP8266_lib
+#include <Ambient.h>
+
+#include <WiFi.h>
 #include <M5Stack.h>
+
+// https://github.com/m5stack/M5Stack/blob/master/examples/Modules/DHT12/DHT12.ino
 #include "DHT12.h"
 #include <Wire.h>     //The DHT12 uses I2C comunication.
 DHT12 dht12;          //Preset scale CELSIUS and ID 0x5c.
-
 TFT_eSprite img = TFT_eSprite(&M5.Lcd);
+
+#include "WiFiSSID.h"
+const char* ssid     = MY_WIFI_SSID;          // SSID は WiFiSSID.h に書く： #define MY_WIFI_SSID "ABCD" 
+const char* password = MY_WIFI_PASSWORD;      // パスワード は WiFiSSID.h  に書く： #define MY_WIFI_PASSWORD "ABCD" 
+
+Ambient ambient;
+WiFiClient ambientWiFiClient;
+unsigned int ambientChannelId = AMBIENT_CHANNEL_ID;   // WiFiSSID.h に書く：#define AMBIENT_CHANNEL_ID 10000000
+const char* ambientWriteKey  = AMBINET_WRITE_KEY;     // WiFiSSID.h に書く：#define AMBINET_WRITE_KEY "abcd12345"
+
+// WiFi接続確認とAmbientへの接続
+bool IsWiFiConnected()
+{
+  static bool bConnected = false;
+
+  if(bConnected) {
+    if (WiFi.status() != WL_CONNECTED) {
+      bConnected = false;
+    }
+  }else {
+    if (WiFi.status() == WL_CONNECTED) {
+      bConnected = true;
+      ambient.begin(ambientChannelId, ambientWriteKey, &ambientWiFiClient);
+    }
+  }
+}
 
 void setup() {
   M5.begin();
@@ -14,6 +65,7 @@ void setup() {
   img.pushSprite(0, 0);
   
   M5.Lcd.setBrightness(60);
+  WiFi.begin(ssid, password);
 }
 
 inline
@@ -30,20 +82,27 @@ void loop() {
 
   // 温度計のメモリと針
   {
+    const int16_t max_angle=140;  // メモリの角度範囲　140度
+
     int16_t i;
-    int16_t cx,cy, r; // 温度計針とメモリの中央の座標とメモリの大きさr
+    int16_t cx,cy, r; // 温度計針とメモリの中央の座標とメモリの大きさ
     cx = M5.Lcd.width()/2;
     cy = M5.Lcd.height()/2;
     r = cy*7/8;
-    int16_t max_angle=140;  // メモリの角度範囲　140度
 
-    // 温度計の目盛り 15個　1度単位
+    // 温度計の目盛り 15個　温度1度単位
     for(i = 0; i <= max_angle; i+=10) {
       img.drawLine(
         rl2x(i-max_angle-(180-max_angle)/2, r*4/5, cx,cy), rl2y(i-max_angle-(180-max_angle)/2, r*4/5, cx,cy),
         rl2x(i-max_angle-(180-max_angle)/2, r, cx,cy), rl2y(i-max_angle-(180-max_angle)/2, r, cx,cy),
         TFT_WHITE);
     }
+
+    // 角度 -max_angle-(180-max_angle)/2 は 　　0度の角度のを補正している。補正量0で→。補正量90度で↓，補正量180度で←。補正量270度↑
+    // max_angleは140度。とりあえずメータのメモリ幅分140度分右に回すことで0度の位置を目盛りの左端。
+    // 目盛り自体のゼロ位置が少し傾いているのでその分を計算しているのが -(180-max_angle)/2 。180-140=40度。左右のそれぞれの目盛りが水平から傾いている
+    // 分の合計なので半分にして　40度÷2で20度の右にまわす。
+    // 右向きに 140+20で160度回している。これで，左端のメモリ
 
 
     // 温度計の針を描画
@@ -69,13 +128,19 @@ void loop() {
   img.setTextColor(TFT_WHITE, TFT_BLUE);
   img.setFreeFont(&FreeSans24pt7b);
 
+
+  float nowTemperature, nowHumidity;
+
+  nowTemperature = dht12.readTemperature();
+  nowHumidity = dht12.readHumidity();
+
 /*
   // 中央に２行
   {
     char buf[20];
-    sprintf(buf, "%.1fC", dht12.readTemperature());
+    sprintf(buf, "%.1fC", nowTemperature);
     img.drawString(buf, M5.Lcd.width()/2, M5.Lcd.height()/3, 1);
-    sprintf(buf, "%.1f%%", dht12.readHumidity());
+    sprintf(buf, "%.1f%%", nowHumidity);
     img.drawString(buf, M5.Lcd.width()/2, M5.Lcd.height()*2/3, 1);
   }
 */
@@ -83,9 +148,9 @@ void loop() {
   // したの方に2行  
   {
     char buf[20];
-    sprintf(buf, "%.1fC", dht12.readTemperature());
+    sprintf(buf, "%.1fC", nowTemperature);
     img.drawString(buf, M5.Lcd.width()/2, M5.Lcd.height()*3/5, 1);
-    sprintf(buf, "%.1f%%", dht12.readHumidity());
+    sprintf(buf, "%.1f%%", nowHumidity);
     img.drawString(buf, M5.Lcd.width()/2, M5.Lcd.height()*5/6, 1);
   
   }
@@ -94,11 +159,18 @@ void loop() {
   // 一番下に1行
   {
     char buf[20];
-    sprintf(buf, "%.1fC %.1f%%", dht12.readTemperature(), dht12.readHumidity());
+    sprintf(buf, "%.1fC %.1f%%", nowTemperature, nowHumidity);
     img.drawString(buf, M5.Lcd.width()/2, M5.Lcd.height()*4/5, 1);
   }
 */
 
   img.pushSprite(0, 0);
-  delay(5000);
+  delay(2500);
+  // 描画とWiFi通信を同時にすると，描画が遅くなってチカチカするので，時間差(2.5秒差)で処理
+  if(IsWiFiConnected()) {
+    ambient.set(1, nowTemperature); 
+    ambient.set(2, nowHumidity);
+    ambient.send();
+  }  
+  delay(2500);
 }
